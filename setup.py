32786e15a2557fc1f7fcf2a4026365a94910cb79
Kleyson Rios <kleyson-sr@segplan.go.gov.br>
2019-01-30 08:42:59 -0200

    - add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# https://python-packaging.readthedocs.io/en/latest/minimal.html
+# python3 setup.py register sdist upload
+
+from setuptools import setup
+
+setup(name='snsdl',
+      version='0.0.1',
+      description='Deep Learning helpers and mlflow integration',
+      url='https://github.com/kleysonr/snsdl',
+      author='Kleyson Rios',
+      author_email='kleysonr@gmail.com',
+      packages=['snsdl'],
+      zip_safe=False)
