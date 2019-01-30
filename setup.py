557802cc91c4dec312552f32a5881381b2d04984
Kleyson Rios <kleyson-sr@segplan.go.gov.br>
2019-01-30 14:16:52 -0200

    - new pip package version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='snsdl',
-      version='0.0.1',
+      version='0.0.2',
       description='Deep Learning helpers and mlflow integration',
       url='https://github.com/kleysonr/snsdl',
       author='Kleyson Rios',
