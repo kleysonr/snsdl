652c3b1943a2a914be745dce0c2ae7910fc41ea4
Kleyson Rios <kleysonr@yahoo.com.br>
2019-02-16 18:36:57 -0200

    - refactoring to levarege the splitds split functions

--- snsdl/keras/generators/img_fs_batch_generator.py
+++ snsdl/keras/generators/img_fs_batch_generator.py
@@ -1,13 +1,15 @@
 import math, os
 import numpy as np
 from snsdl.utils import paths
+from snsdl.utils.splitds import SplitDataset
 from snsdl.keras.generators.base import ImgFsSequenceGenerator
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer, LabelEncoder
+from sklearn.utils import shuffle as skshuffle
 
 class ImgFsBatchGenerator():
     
-    def __init__(self, dataset_path, test_ratio=0.25, val_ratio=0.0, batch_size=32, binary_classification=False, shuffle=False, preprocessors=[]):
+    def __init__(self, dataset_path, balanced=False, test_ratio=0.25, val_ratio=0.0, batch_size=32, binary_classification=False, shuffle=False, preprocessors=[]):
         """
         Class constructor.
 
@@ -37,13 +39,13 @@ class ImgFsBatchGenerator():
         self.binary_classification = binary_classification
         self.preprocessors = preprocessors
         self.shuffle = shuffle
+        self.balanced = balanced
 
         self.trainGenerator = None
         self.testGenerator = None
         self.valGenerator = None
 
-        # Dict mapping classes and images path
-        self.data = {}
+        self._data = SplitDataset.previewSplit(self.dataset_path, balanced=self.balanced, test_ratio=self.test_ratio, val_ratio=self.val_ratio, shuffle=self.shuffle, type='img')
 
         # Dict for train, test and val dataset
         self.train_test_val = {'train': [], 'test': [], 'val':[]}
@@ -54,55 +56,8 @@ class ImgFsBatchGenerator():
         # Lenght of the dataset
         self.datasetsize = 0
 
-        # Number of images of the smallest class
-        self.minsize = math.inf
-
-        # Mapping between class name and labels enconding {class_name:class_code}
-        self.encoded_classes = None
-
-        # Mapping between class name its encoded index {class_name:class_index}
-        self.class_indices = None
-
         # Encoder
         self.le = None
-
-        # Get a list of all the images under dataset/{class}/*
-        fileslist = paths.list_images(dataset_path)
-
-        for file in fileslist:
-
-            # Extract the label
-            label = file.split(os.path.sep)[-2]
-
-            # Populate dict mapping
-            try:
-                self.data[label]
-            except KeyError:
-                self.data[label] = []
-            finally:
-                self.data[label].append(file)
-                self.datasetsize += 1
-
-        # Loop over each class
-        for k in self.data.keys():
-
-            # Save the size of the smallest class
-            self.minsize = len(self.data[k]) if len(self.data[k]) < self.minsize else self.minsize
-
-        # Calculate the offset where test samples begins, based on the smallest class.
-        # Force to have balanced classes for training.
-        self.offset = int(self.minsize * (1.0 - self.test_ratio))                
-    
-        # Split the full dataset in train and test datasets
-        self.__split_train_test()
-
-        # Split train dataset to generate the validation dataset
-        if self.val_ratio > 0:
-
-            X_train, X_val, _, _ = train_test_split(self.train_test_val['train'], self.train_test_val['train'], test_size=val_ratio)
-
-            self.train_test_val['train'] = X_train
-            self.train_test_val['val'] = X_val
 
         # Encode labels
         if binary_classification:
@@ -111,56 +66,27 @@ class ImgFsBatchGenerator():
             self.le = LabelBinarizer()
 
         # Encode classes
-        classes_name = list(self.data.keys())
+        classes_name = list(self._data.keys())
         class_codes = self.le.fit_transform(classes_name)
 
         # Create mappings
         self.encoded_classes = dict(zip(classes_name, self.le.fit_transform(classes_name)))
         self.class_indices = dict(zip(classes_name, list(np.argmax(class_codes, axis=-1))))        
 
-        # Create dataset labels
-        self.__create_labels()
+        # Re-organize the list of files
+        for label in self._data.keys():
+            for ds in self._data[label]:
+                for f in self._data[label][ds]:
+                    self.train_test_val[ds].append(f)
+                    self.labels_train_test_val[ds].append(self.encoded_classes[label])
+                    self.datasetsize += 1
+
+        # Shuffle the arrays
+        self.train_test_val['train'], self.labels_train_test_val['train'] = skshuffle(self.train_test_val['train'], self.labels_train_test_val['train'], random_state=0)
+        self.train_test_val['test'], self.labels_train_test_val['test'] = skshuffle(self.train_test_val['test'], self.labels_train_test_val['test'], random_state=0)
+        self.train_test_val['val'], self.labels_train_test_val['val'] = skshuffle(self.train_test_val['val'], self.labels_train_test_val['val'], random_state=0)
 
         self.__info()
-
-    def __create_labels(self):
-
-        # Loop over each dataset
-        for ds in self.train_test_val.keys():
-
-            # Size of elements in the dataset
-            size = len(self.train_test_val[ds])
-
-            labels = []
-            for i in range(0, size):
-
-                # Class name
-                label = self.train_test_val[ds][i].split(os.path.sep)[-2]
-                label = self.encoded_classes[label]
-                labels.append(label)
-
-            self.labels_train_test_val[ds] = dict(zip(self.train_test_val[ds], labels))
-
-    def __split_train_test(self):
-
-        _train = []
-        _test = []
-
-        # Loop over each class
-        for k in self.data.keys():
-
-            # Shuffle the images in each class
-            items = self.data[k]
-            np.random.shuffle(items)
-
-            _train += items[:self.offset]
-            _test  += items[self.offset:]
-
-        np.random.shuffle(_train)
-        np.random.shuffle(_test)
-
-        self.train_test_val['train'] = _train
-        self.train_test_val['test'] = _test
 
     @property
     def train(self):
@@ -191,7 +117,7 @@ class ImgFsBatchGenerator():
 
     def getNumberOfClasses(self):
         """Get the number of training classes."""
-        return len(self.data.keys())
+        return len(self._data.keys())
 
     def getDatasetSize(self, dataset):
         """Get the size of a given dataset (full / train / test / val)."""
@@ -204,7 +130,7 @@ class ImgFsBatchGenerator():
     def getTrueClasses(self, dataset):
         """Get an array with the true classes for a given dataset (train / test / val)."""
 
-        encoded = [self.labels_train_test_val[dataset][s] for s in self.train_test_val[dataset]]
+        encoded = [self.labels_train_test_val[dataset][i] for i, s in enumerate(self.train_test_val[dataset])]
         encoded_indx = list(np.argmax(encoded, axis=-1))
 
         return [list(self.class_indices.keys())[list(self.class_indices.values()).index(s)] for s in encoded_indx]
