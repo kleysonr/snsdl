df7a6aea46eecdce31962248202d37f1ef353426
Kleyson Rios <kleysonr@yahoo.com.br>
2019-02-16 18:44:22 -0200

    - atualizacao pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 # https://python-packaging.readthedocs.io/en/latest/minimal.html
 # python3 setup.py register sdist upload
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='snsdl',
-      version='0.0.2',
+      version='0.0.3',
       description='Deep Learning helpers and mlflow integration',
       url='https://github.com/kleysonr/snsdl',
       author='Kleyson Rios',
       author_email='kleysonr@gmail.com',
-      packages=['snsdl'],
+      packages=find_packages(),
       zip_safe=False)
