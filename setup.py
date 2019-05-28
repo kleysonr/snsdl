2f3cc61d585774f1cb2513b6507e7b872f9eb888
Kleyson Rios <kleysonr@yahoo.com.br>
2019-05-28 16:38:56 -0300

    - pypi 0.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='snsdl',
-      version='0.0.3',
+      version='0.0.4',
       description='Deep Learning helpers and mlflow integration',
       url='https://github.com/kleysonr/snsdl',
       author='Kleyson Rios',
